31dd750f880e1a203dfcd9e3ef83fb92004a47a8
Eelvex <eelvex@gmail.com>
2012-03-09 09:33:34 +0200

    test

---
+++ wp-content/plugins/nextgen-gallery/xml/media-rss.php
@@ -0,0 +1,131 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss/' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [http://nextgen-gallery.com]]]></generator>
+		<title>elements</title>
+		<description>Science, love, life and madness.</description>
+		<link><![CDATA[http://www.eelvex.net]]></link>
+		<atom:link rel='self' href='http://www.eelvex.net/wp-content/plugins/nextgen-gallery/xml/media-rss.php' type='application/rss+xml' />
+		<atom:link rel='next' href='http://www.eelvex.net/wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[stamp]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/graphs/stamp.png]]></link>
+			<guid>image-id:21</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/graphs/stamp.png' medium='image' />
+			<media:title><![CDATA[stamp]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/graphs/thumbs/thumbs_stamp.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[tkzgnupl]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/graphs/tkzgnupl.png]]></link>
+			<guid>image-id:20</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/graphs/tkzgnupl.png' medium='image' />
+			<media:title><![CDATA[tkzgnupl]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/graphs/thumbs/thumbs_tkzgnupl.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[tkz]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/graphs/tkz.png]]></link>
+			<guid>image-id:19</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/graphs/tkz.png' medium='image' />
+			<media:title><![CDATA[tkz]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/graphs/thumbs/thumbs_tkz.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[oth]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/graphs/oth.png]]></link>
+			<guid>image-id:18</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/graphs/oth.png' medium='image' />
+			<media:title><![CDATA[oth]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/graphs/thumbs/thumbs_oth.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[directory_tree]]></title>
+			<description><![CDATA[Directory tree.]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/misc/directory_tree.png]]></link>
+			<guid>image-id:17</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/misc/directory_tree.png' medium='image' />
+			<media:title><![CDATA[directory_tree]]></media:title>
+			<media:description><![CDATA[Directory tree.]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/misc/thumbs/thumbs_directory_tree.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[screenshot]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/misc/screenshot.png]]></link>
+			<guid>image-id:16</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/misc/screenshot.png' medium='image' />
+			<media:title><![CDATA[screenshot]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/misc/thumbs/thumbs_screenshot.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[triangle_pout]]></title>
+			<description><![CDATA[External point.]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/geometry/triangle_pout.png]]></link>
+			<guid>image-id:15</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/geometry/triangle_pout.png' medium='image' />
+			<media:title><![CDATA[triangle_pout]]></media:title>
+			<media:description><![CDATA[External point.]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/geometry/thumbs/thumbs_triangle_pout.png' width='150' height='150' />
+			<media:keywords><![CDATA[triangle inner]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[triangle_pin]]></title>
+			<description><![CDATA[Internal point.]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/geometry/triangle_pin.png]]></link>
+			<guid>image-id:14</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/geometry/triangle_pin.png' medium='image' />
+			<media:title><![CDATA[triangle_pin]]></media:title>
+			<media:description><![CDATA[Internal point.]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/geometry/thumbs/thumbs_triangle_pin.png' width='150' height='150' />
+			<media:keywords><![CDATA[triangle inner]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[hexagonal_lattice]]></title>
+			<description><![CDATA[A hexagonal lattice.]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/brillouin_zone_hex/hexagonal_lattice.png]]></link>
+			<guid>image-id:11</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/brillouin_zone_hex/hexagonal_lattice.png' medium='image' />
+			<media:title><![CDATA[hexagonal_lattice]]></media:title>
+			<media:description><![CDATA[A hexagonal lattice.]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/brillouin_zone_hex/thumbs/thumbs_hexagonal_lattice.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[bragg_planes_hexagonal]]></title>
+			<description><![CDATA[Bragg planes of the hexagonal lattice.]]></description>
+			<link><![CDATA[http://www.eelvex.net/wp-content/gallery/brillouin_zone_hex/bragg_planes_hexagonal.png]]></link>
+			<guid>image-id:10</guid>
+			<media:content url='http://www.eelvex.net/wp-content/gallery/brillouin_zone_hex/bragg_planes_hexagonal.png' medium='image' />
+			<media:title><![CDATA[bragg_planes_hexagonal]]></media:title>
+			<media:description><![CDATA[Bragg planes of the hexagonal lattice.]]></media:description>
+			<media:thumbnail url='http://www.eelvex.net/wp-content/gallery/brillouin_zone_hex/thumbs/thumbs_bragg_planes_hexagonal.png' width='150' height='150' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) elements (http://www.eelvex.net)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
